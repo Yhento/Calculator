84685d4df23b05d42cb4b03e1e3ebe85557c1a15
darth <154254574+Yhento@users.noreply.github.com>
2024-03-26 07:44:04 -0500

    add html and basic math functions

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const add = (num1, num2) => {
+
+  return num1 + num2;
+
+}
+
+const subtract = (num1, num2) => {
+
+  return num1 - num2;
+
+}
+
+const multiply = (num1, num2) => {
+
+  return num1 * num2;
+
+}
+
+const divide = (num1, num2) => {
+
+  return num1 / num2;
+
+}
+
+console.log(divide(25,5));
