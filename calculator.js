3f7a15f318eef71d3fb6fdd5ba1034d23b0335d5
darth <154254574+Yhento@users.noreply.github.com>
2024-03-27 06:24:36 -0500

    feat: create populateDisplay() function.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 const add = (num1, num2) => {
-
+  console.log(num1, num2)
   return num1 + num2;
 
 }
@@ -17,14 +17,19 @@ const multiply = (num1, num2) => {
 }
 
 const divide = (num1, num2) => {
-
-  return num1 / num2;
+  if (num2 == 0){
+    return "lmao";
+  } else {
+  return (num1 / num2);
+  }
 
 }
 
-let number_one = 0;
-let number_two = 0;
+let number_one = "";
+let number_two = "";
+let user_input = 0;
 let operator = "";
+
 
 const operate = (operator, number_one, number_two) => {
 
@@ -49,4 +54,44 @@ const operate = (operator, number_one, number_two) => {
   }
 
 }
+
+var calculator = document.querySelector(".calculator");
+var calculator_display = document.querySelector(".display");
+var history = document.querySelector(".history span");
+calculator_display.value = 0;
+
+const populateDisplay = (text) => {
+
+  if (user_input == 0 && text == 0) {
+    user_input = text;
+    calculator_display.value = text;
+  }
+  else if (user_input == 0 && text !== "." && !user_input.includes(".")) {
+    user_input = text;
+    calculator_display.value = user_input;
+  }
+  else {
+    user_input += text;
+    calculator_display.value = user_input;
+  }
+
+}
+
+populateDisplay("0");
+console.log(user_input);
+populateDisplay(".");
+console.log(user_input);
+populateDisplay("5");
+console.log(user_input);
+populateDisplay("5");
+console.log(user_input);
+
+
+
+
+
+
+
+
+
 
